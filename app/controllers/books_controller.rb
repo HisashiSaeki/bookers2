0d28c12e07f93045282b16379829ec4a6ba5f811
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-09-04 06:32:41 +0000

    [Finish] ログイン画面に遷移＆投稿ユーザーのみボタン表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:update] = "You have updated book successfully."
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
